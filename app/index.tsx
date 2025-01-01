03b5626ce629934888ee86b213362f709f7042f1
keyopto <keyopto@gmail.com>
2025-01-01 16:45:53 +0100

    chore: add eslint, prettier, husky to project

---
+++ app/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+export default function Index() {
+  return (
+    <View style={styles.container}>
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+    flex: 1,
+    justifyContent: 'center',
+  },
+});
---
+++ eslint.config.mjs
@@ -0,0 +1,37 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "@typescript-eslint/eslint-plugin";
+import pluginReact from "eslint-plugin-react";
+import eslintPluginReactNative from "eslint-plugin-react-native";
+import eslintConfigPrettier from "eslint-config-prettier";
+import tsParser from "@typescript-eslint/parser";
+import eslintPluginPrettier from "eslint-plugin-prettier";
+
+export default [
+  {
+    files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"],
+    languageOptions: {
+      globals: globals.node,
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: { jsx: true },
+      },
+    },
+    plugins: {
+      ['react']: pluginReact,
+      ['react-native']: eslintPluginReactNative,
+      ['@typescript-eslint']: tseslint,
+      ['prettier']: eslintPluginPrettier
+    },
+    rules: {
+      "prettier/prettier": "error",
+      ...pluginJs.configs.recommended.rules,
+      ...pluginReact.configs.recommended.rules,
+      ...tseslint.configs.recommended.rules,
+      ...eslintPluginReactNative.configs.all.rules,
+      ...eslintConfigPrettier.rules,
+    },
+  },
+];
