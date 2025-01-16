301769a96536b526955344163f7e9e96e08e05b9
Keyopto <keyopto@gmail.com>
2025-01-16 11:06:43 +0100

    feat: form date should not be in the past

--- types/forms/AddGoalForm.tsx
+++ types/forms/AddGoalForm.tsx
@@ -5,9 +5,15 @@ const AddGoalForm = (t: (key: string) => string) => {
     title: z
       .string({ required_error: t('validation.title.required') })
       .min(1, t('validation.title.required')),
-    date: z.date({
-      required_error: t('validation.date.required'),
-    }),
+    date: z
+      .date({
+        required_error: t('validation.date.required'),
+      })
+      .refine((date) => {
+        const today = new Date();
+        today.setHours(0, 0, 0, 0);
+        return date.getTime() >= today.getTime();
+      }, t('validation.date.past')),
     description: z
       .string({ required_error: t('validation.description.required') })
       .min(1, t('validation.description.required')),
